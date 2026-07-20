8a373d4907d8ca95fff0ed0f4044172acf0a425a
Hermes <hermes@openclaw>
2026-07-20 21:46:21 +0100

    fix: update pytest-asyncio configuration

--- conftest.py
+++ conftest.py
@@ -6,6 +6,12 @@ def configure_appdaemontestframework_for_pytester(testdir):
     """
     Extra test fixtue use for testing pytest runners.
     """
+    testdir.makeini(
+        """
+        [pytest]
+        asyncio_default_fixture_loop_scope = function
+    """
+    )
     testdir.makeconftest(
         """
         from appdaemontestframework.pytest_conftest import *
